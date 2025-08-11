b3d4f88bc2f6aa9d4bd845398b5a270a3e751638
shifa-imran-ali <shifaimran523@gmail.com>
2025-08-11 20:47:24 +0500

    donee

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ export default function Login() {
     e.preventDefault();
     setError("");
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", formData);
+      const res = await axios.post("https://heckathon-backend-3.onrender.com/api/auth/login", formData, { withCredentials: true });
       localStorage.setItem("token", res.data.token);
       navigate("/Landing");
     } catch (err) {
